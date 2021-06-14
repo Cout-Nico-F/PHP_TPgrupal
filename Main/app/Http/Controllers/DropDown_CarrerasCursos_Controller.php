3c508ec978cfa395cdd414572d459c3db3c1be9f
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-14 17:22:36 -0300

    movi logica desde Dao hacia Controller

--- Main/app/Http/Controllers/DropDown_CarrerasCursos_Controller.php
+++ Main/app/Http/Controllers/DropDown_CarrerasCursos_Controller.php
@@ -7,21 +7,24 @@ class DropDown_CarrerasCursos_Controller extends Controller
     //metodo que recibe el array de objetos del dao y lo mapea en un objeto comprensible para la capa view.
     public static function BuscarCarreraController($idAlumno){
         
-        $resultados = \App\Models\Consultas::BuscarCarreraDao($idAlumno);//este metodo ahora esta en controller pero deberiamos moverlo a una carpeta Dao
+        $resultados = \App\Models\Consultas::BuscarCarreraDao($idAlumno);
 
-        // if($resultados == -1){
-        //     //"la consulta trajo mas de un resultado, se esperaba uno"
-        //     return -1;
-        // }
-        // else if($resultados == 0){
-        //     //"La consulta carrera no trajo un resultado"
-        //     return 0;
-        // }
+        $row = pg_num_rows($resultados); //Retorna un entero y un -1 en caso de fallo
+
+        if($row == -1){ //Si no trajo nada
+            die("La consulta carrera no trajo un resultado");//TODO: die
+        }
         return $resultados;    
     }
 
     public static function BuscarDeudasController($idalucarrcurs){
-        $resultados = \App\Models\Consultas::BuscarDeudasDao($idalucarrcurs);//este metodo ahora esta en controller pero deberiamos moverlo a una carpeta Dao
+        $resultados = \App\Models\Consultas::BuscarDeudasDao($idalucarrcurs);
+
+        $row = pg_num_rows($resultados); //Retorna un entero y un -1 en caso de fallo
+        if($row == -1){ //Si no trajo nada
+           die("La consulta deuda no trajo un resultado");
+        }
+
         return $resultados;
     }
 }
--- Main/app/Models/Consultas.php
+++ Main/app/Models/Consultas.php
@@ -18,11 +18,7 @@ class Consultas extends Model
         carrerascursosalta.ccal_descripcion FROM (alumnos INNER JOIN alumnoscarreracurso ON alumnos.alum_idalumno = alumnoscarreracurso.alcc_idalumno)
         INNER JOIN carrerascursosalta ON alumnoscarreracurso.alcc_idccalta = carrerascursosalta.ccal_idccalta WHERE alumnos.alum_idalumno='$user'"; 
         $resultado = pg_query($conn,$consulta_carr) or die("Error en la consulta carreras"); //Esto lo puedo hacer porque pg_query devuelve un boolean
-        $row = pg_num_rows($resultado); //Retorna un entero y un -1 en caso de fallo
-
-        if($row == -1){ //Si no trajo nada
-            die("La consulta carrera no trajo un resultado");
-        }
+        
         pg_close();
         return $resultado;//TODO: cuidado aca. este casteo esta ok?
     }
@@ -37,10 +33,7 @@ class Consultas extends Model
         WHERE ((pagoalumnos.paal_idalucarrcurs='$idalucarrcurs') AND (pagoalumnos.paal_estado <>'PAGO')) ORDER BY pagoalumnos.paal_fechadeb";
 
         $resultado = pg_query($conn,$consulta_deuda) or die("Error en la consulta deuda"); //Esto lo puedo hacer porque pg_query devuelve un boolean
-        $row = pg_num_rows($resultado); //Retorna un entero y un -1 en caso de fallo
-        if($row == -1){ //Si no trajo nada
-           die("La consulta deuda no trajo un resultado");
-        }
+        
         pg_close();
         return $resultado;  
     }
